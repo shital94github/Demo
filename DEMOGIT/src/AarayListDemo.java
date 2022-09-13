7b4a105bb0966a3a613800416c2d586900afef73
shital tidke <shital.tidke09@gmail.com>
2022-09-13 18:01:46 +0530

    arralist is added

---
+++ DEMOGIT/src/AarayListDemo.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class AarayListDemo {
+
+	public static void main(String[] args) {
+		
+List<String>list= Arrays.asList("Shita","rahul","pihu","SAMU","JUNU");
+Iterator<String> iterator = list.iterator();
+while(iterator.hasNext()) {
+	System.out.println(iterator.next());
+}
+	}
+
+}
